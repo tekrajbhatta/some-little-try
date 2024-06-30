8836b9b8b473a36c030b46bac235d380d0e9b324
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-07-01 00:12:54 +0545

    Added footer section design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -794,6 +794,171 @@ use Illuminate\Support\Str;
             color: #fff !important;
         }
 
+
+        /* Footer CSS */
+        footer {
+  /* position: absolute;
+
+  bottom: 0;
+
+  left: 0;
+
+  right: 0; */
+
+  background: #111;
+
+  height: auto;
+
+  width: 100vw;
+
+  padding-top: 40px;
+
+  color: #fff;
+}
+
+.footer-content {
+  display: flex;
+
+  align-items: center;
+
+  justify-content: center;
+
+  flex-direction: column;
+
+  text-align: center;
+}
+
+.footer-content h3 {
+  font-size: 2.1rem;
+
+  font-weight: 500;
+
+  text-transform: capitalize;
+
+  line-height: 3rem;
+}
+
+.footer-content p {
+  max-width: 500px;
+
+  margin: 10px auto;
+
+  line-height: 28px;
+
+  font-size: 14px;
+
+  color: #cacdd2;
+}
+
+.socials {
+  list-style: none;
+
+  display: flex;
+
+  align-items: center;
+
+  justify-content: center;
+
+  margin: 1rem 0 3rem 0;
+}
+
+.socials li {
+  margin: 0 10px;
+}
+
+.socials a {
+  text-decoration: none;
+
+  color: #fff;
+
+  padding: 5px;
+}
+
+.socials a i {
+  font-size: 1.1rem;
+
+  width: 20px;
+
+  transition: color 0.4s ease;
+}
+
+.socials a:hover i {
+  color: aqua;
+}
+
+.footer-bottom {
+  background: #000;
+
+  width: 100vw;
+
+  padding: 20px;
+
+  padding-bottom: 40px;
+
+  text-align: center;
+}
+
+.footer-bottom p {
+  float: left;
+
+  font-size: 14px;
+
+  word-spacing: 2px;
+}
+
+.footer-bottom p a {
+  color: #ccc;
+
+  font-size: 16px;
+
+  text-decoration: none;
+}
+
+.footer-bottom span {
+  text-transform: uppercase;
+}
+
+.footer-menu {
+  float: right;
+}
+
+.footer-menu ul {
+  display: flex;
+}
+
+.footer-menu ul li {
+  padding-right: 10px;
+
+  display: block;
+}
+
+.footer-menu ul li a {
+  color: #cfd2d6;
+
+  text-decoration: none;
+}
+
+.footer-menu ul li a:hover {
+  color: #27bcda;
+}
+
+@media (max-width: 500px) {
+  .footer-bottom p {
+    float: none;
+  }
+
+  .footer-menu ul {
+    display: flex;
+
+    margin-top: 10px;
+
+    margin-bottom: 20px;
+
+    text-align: center;
+  }
+}
+
+
     </style>
 </head>
 
@@ -954,10 +1119,74 @@ use Illuminate\Support\Str;
 
     </div>
 
+    
+    <div class="" style="width:100%; background-color: #37373d; padding: 6px 0;">
+            <div class="container">
+                <div class="contact-info">
+                    <span>You may have anything:</span>
+                </div>
+            </div>
+        </div>
+
+    <footer>
+
+        <div class="footer-content">
+
+        <h3>Footer Example</h3>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae felis scelerisque, gravida sapien non, cursus augue. Aenean id pretium turpis. Suspendisse eros nunc, sollicitudin nec.</p>
+
+        <ul class="socials">
+
+            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+
+            <li><a href="#"><i class="bi bi-twitter"></i></a></li>
+
+            <li><a href="#"><i class="bi bi-google-plus"></i></a></li>
+
+            <li><a href="#"><i class="bi bi-youtube"></i></a></li>
+
+            <li><a href="#"><i class="bi bi-linkedin-square"></i></a></li>
+
+        </ul>
+
+        </div>
+
+        <div class="footer-bottom">
+
+        <p>Copyright &copy <span id="year"></span> <a href="#">@coding_dev_</a> </p>
+
+        <div class="footer-menu">
+
+            <ul class="f-menu">
+
+            <li><a href="">Home</a></li>
+
+            <li><a href="">About</a></li>
+
+            <li><a href="">Contact</a></li>
+
+            <li><a href="">Blog</a></li>
+
+            </ul>
+
+        </div>
+
+        </div>
+
+</footer>
+        
+
     <!-- jQuery CDN -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+    <!-- Footer JS -->
+    <script>
+        const year = document.getElementById("year");
+        year.textContent = new Date().getFullYear();
+    </script>
 
     <!-- JS to move boxes to specific section -->
     <script>
